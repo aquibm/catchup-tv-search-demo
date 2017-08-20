ea9db39e53e32c337bd9df41fe7fc3264197d8d3
Aquib Master <aquibmaster@gmail.com>
2017-08-20 18:48:56 +1200

    Build out search

--- src/App.js
+++ src/App.js
@@ -1,21 +1,55 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    state = {
+        searchValue: '',
+        searchResults: []
+    }
+
+    _onSearchInput = async event => {
+        const value = event.target.value
+        let results = []
+
+        if (value) {
+            const response = await fetch(`http://localhost:3000?q=${value}`)
+            const data = await response.json()
+            results = data.hits.hits.map(hit => hit._source)
+        }
+
+        this.setState({
+            searchValue: value,
+            searchResults: results
+        })
+    }
+
+    render() {
+        const { searchValue, searchResults } = this.state
+
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <input
+                        className="App-input"
+                        type="text"
+                        placeholder="Search for TV shows"
+                        value={searchValue}
+                        onChange={this._onSearchInput}
+                        autoFocus
+                    />
+                </div>
+
+                <div>
+                    {searchResults.map(result =>
+                        <div key={result.id}>
+                            {result.name}
+                        </div>
+                    )}
+                </div>
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
